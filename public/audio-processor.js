a5acd3524a6e7d451b632b6b341373c52d5bb109
nguyen-thanh-viet <vietqn1987@gmail.com>
2026-06-25 23:00:25 +0700

    Fix audio chunking to reduce latency

--- public/audio-processor.js
+++ public/audio-processor.js
@@ -1,18 +1,27 @@
 class AudioProcessor extends AudioWorkletProcessor {
+  constructor() {
+    super();
+    this.bufferSize = 4096; // 256ms of audio at 16000Hz
+    this.buffer = new Int16Array(this.bufferSize);
+    this.bufferIndex = 0;
+  }
+
   process(inputs, outputs, parameters) {
     const input = inputs[0];
     if (input && input.length > 0) {
       const channelData = input[0];
       if (channelData) {
-        // Convert Float32Array (-1.0 to 1.0) to Int16Array (-32768 to 32767)
-        const pcmData = new Int16Array(channelData.length);
         for (let i = 0; i < channelData.length; i++) {
+          // Convert Float32Array (-1.0 to 1.0) to Int16Array (-32768 to 32767)
           let s = Math.max(-1, Math.min(1, channelData[i]));
-          pcmData[i] = s < 0 ? s * 0x8000 : s * 0x7FFF;
+          this.buffer[this.bufferIndex++] = s < 0 ? s * 0x8000 : s * 0x7FFF;
+          
+          if (this.bufferIndex >= this.bufferSize) {
+            // Post the buffered PCM data back to the main thread
+            this.port.postMessage(new Int16Array(this.buffer));
+            this.bufferIndex = 0;
+          }
         }
-        
-        // Post the PCM data back to the main thread
-        this.port.postMessage(pcmData);
       }
     }
     // Return true to keep the processor alive
